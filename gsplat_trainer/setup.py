6049afaa87f165662e14a017407a5dcc5b42233e
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-11-17 15:32:29 +0100

    Switch to minimum dependencies for setup

--- gsplat_trainer/setup.py
+++ gsplat_trainer/setup.py
@@ -9,12 +9,12 @@ setup(
         "torch>=2.0.1",
         "numpy>=1.21.1",
         "pandas>=1.3.3",
-        "tqdm==2.2.3",
-        "wandb==0.17.0",
-        "pytest==8.3.3",
-        "plyfile==1.1",
-        "pillow==11.0.0",
-        "torchvision==0.20.1",
-        "matplotlib==3.9.2"
+        "tqdm>=2.2.3",
+        "wandb>=0.17.0",
+        "pytest>=8.3.3",
+        "plyfile>=1.1",
+        "pillow>=11.0.0",
+        "torchvision>=0.20.1",
+        "matplotlib>=3.9.2"
     ],
 )
