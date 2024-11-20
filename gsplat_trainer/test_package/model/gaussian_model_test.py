dc8b659e58c033b7d68c256dc27b867d2742424c
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-11-20 10:12:18 +0100

    Rewrite device test for model

--- gsplat_trainer/test_package/model/gaussian_model_test.py
+++ gsplat_trainer/test_package/model/gaussian_model_test.py
@@ -26,14 +26,24 @@ class GaussianModelTest(unittest.TestCase):
         self.gaussian_model = MockFactory.create_mocked_gaussian_model(
             n_points=self.pdc_points
         ).to(self.device)
-        self.assertTrue(str(self.gaussian_model.params["means"].device) == self.device)
-        self.assertTrue(str(self.gaussian_model.params["quats"].device) == self.device)
-        self.assertTrue(str(self.gaussian_model.params["scales"].device) == self.device)
         self.assertTrue(
-            str(self.gaussian_model.params["opacities"].device) == self.device
+            self.gaussian_model.params["means"].device == torch.device(self.device)
         )
-        self.assertTrue(str(self.gaussian_model.params["sh0"].device) == self.device)
-        self.assertTrue(str(self.gaussian_model.params["shN"].device) == self.device)
+        self.assertTrue(
+            self.gaussian_model.params["quats"].device == torch.device(self.device)
+        )
+        self.assertTrue(
+            self.gaussian_model.params["scales"].device == torch.device(self.device)
+        )
+        self.assertTrue(
+            self.gaussian_model.params["opacities"].device == torch.device(self.device)
+        )
+        self.assertTrue(
+            self.gaussian_model.params["sh0"].device == torch.device(self.device)
+        )
+        self.assertTrue(
+            self.gaussian_model.params["shN"].device == torch.device(self.device)
+        )
 
     def test_given_a_valid_gaussian_model__when_forwarding_with_camera_parameters__then_do_not_throw_an_error(
         self,
