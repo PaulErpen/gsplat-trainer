6961ed34c5f1860691eba254fb8e99f1d8536ee6
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-11-20 09:51:11 +0100

    Attempt to resolve cuda issues

--- gsplat_trainer/gsplat_trainer/model/gaussian_model.py
+++ gsplat_trainer/gsplat_trainer/model/gaussian_model.py
@@ -29,9 +29,10 @@ class GaussianModel(nn.Module):
         colors = torch.zeros((num_points, (sh_degree + 1) ** 2, 3))  # [N, K, 3]
         colors[:, 0, :] = rgb_to_sh(torch.tensor(np.asarray(pcd.colors)).float())
 
-        dist2 = torch.clamp_min(
-            distCUDA2(torch.from_numpy(np.asarray(pcd.points)).float()), 0.0000001
-        )
+        distance_basis = points.clone()
+        if torch.cuda.is_available():
+            distance_basis = distance_basis.cuda()
+        dist2 = torch.clamp_min(distCUDA2(), 0.0000001)
         scales = torch.log(torch.sqrt(dist2))[..., None].repeat(1, 3)
 
         opacities = inverse_sigmoid(0.1 * torch.ones((num_points), dtype=torch.float))
