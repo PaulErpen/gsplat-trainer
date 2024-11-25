130399b347e5544639f3ce2e05ed1a99c7a23860
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-11-25 19:33:23 +0100

    Fix config calls in create_schedulers

--- gsplat_trainer/gsplat_trainer/train/scheduler_factory.py
+++ gsplat_trainer/gsplat_trainer/train/scheduler_factory.py
@@ -1,12 +1,15 @@
 from typing import Dict, List
+from gsplat_trainer.config.config import Config
 import torch
+from torch.optim.lr_scheduler import LRScheduler
+
 
 class SchedulerFactory:
-  @staticmethod
-  def create_schedulers(optimizers: Dict, config: "Config") -> List:
-    schedulers = [
-        torch.optim.lr_scheduler.ExponentialLR(
-          optimizers["means"], gamma=0.01 ** (1.0 / config.max_steps)
-        )
-    ]
-    return schedulers
+    @staticmethod
+    def create_schedulers(optimizers: Dict, config: Config) -> List[LRScheduler]:
+        schedulers = [
+            torch.optim.lr_scheduler.ExponentialLR(
+                optimizers["means"], gamma=0.01 ** (1.0 / config.max_steps)
+            )
+        ]
+        return schedulers
